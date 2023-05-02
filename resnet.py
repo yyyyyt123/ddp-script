ffd4d4e41e103b042b671c7301f2fe26f77fd6a6
yyyyyt <2021614317@qq.com>
2023-05-02 14:18:31 +0800

    remove matplotlib

--- resnet.py
+++ resnet.py
@@ -99,27 +99,14 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse',
 ########################################################################
 # Let us show some of the training images, for fun.
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 # functions to show an image
 
 
-def imshow(img):
-    img = img / 2 + 0.5  # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-
 # get some random training images
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
-
-# show images
-imshow(torchvision.utils.make_grid(images))
-# print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 ########################################################################
 # 2. Define a Convolutional Neural Network
@@ -211,10 +198,6 @@ print('Finished Training')
 
 dataiter = iter(testloader)
 images, labels = dataiter.next()
-
-# print images
-imshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 ########################################################################
 # Okay, now let us see what the neural network thinks these examples above are:
