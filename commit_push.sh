4b342da0b01d43e80ff95d99afb88dd17af82d30
yyyyyt <2021614317@qq.com>
2023-05-02 15:41:21 +0800

    change bs to 64

--- commit_push.sh
+++ commit_push.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+if [ -z "$1" ]
+then
+    echo "please input comment of this commit"
+    exit 1
+fi
 echo $1 
 
 git add .
 git reset run_multinode.sh
-git commit -m "{$1}"
+git commit -m "$1"
+git push origin master
